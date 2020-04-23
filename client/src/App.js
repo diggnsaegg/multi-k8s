505d5c80ef511eeede2a7241443d927ba9753c39
dan <bla@bla.de>
2020-04-23 22:08:59 +0200

    fix App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-            <h1 className="App-title">Fib Calc version 2</h1>
-=======
             <h1 className="App-title">Fib Calc Version2.1</h1>
->>>>>>> b981a7f05158d6d1e9f0fee3c0e885363f3e9045
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
